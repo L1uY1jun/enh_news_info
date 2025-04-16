1eeb424e2ec3747077395ba04697c2133f7591d5
L1uY1jun <yijunlau@gmail.com>
2025-04-16 19:44:30 +0800

    Debug renderer

--- renderer.py
+++ renderer.py
@@ -39,8 +39,7 @@ def render(html_path):
 # Testing-------------------------------------------------------
 if __name__ == "__main__":
     html_url = "sample_html.html"
-    # html_path = "C:\\Users\\Liu Yijun\\Desktop\\Study\\FYP\\eCaption\\enh_news_info\\test\\" + html_url
-    html_path = "C:/Users/Liu Yijun/Desktop/Study/FYP/eCaption/enh_news_info/out/2025_04_08_21_03/layout.html"
+    html_path = "C:\\Users\\Liu Yijun\\Desktop\\Study\\FYP\\eCaption\\enh_news_info\\test\\" + html_url
     
     infographic_img = render(html_path)
-    infographic_img.save("enh_news_info/out/2025_04_08_21_03/init_infographic.png")
+    infographic_img.save("enh_news_info/test/test_infographic.png")
