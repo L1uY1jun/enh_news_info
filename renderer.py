ea19b7530a67d6b2c70579b360a1e4698ac368d7
L1uY1jun <yijunlau@gmail.com>
2025-04-20 00:02:27 +0800

    Update Renderer to improve resolution

--- renderer.py
+++ renderer.py
@@ -8,12 +8,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
+import base64
 import os
 
 load_dotenv()
 CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH")
-CAPTURE_WIDTH = 5000
-CAPTURE_HEIGHT = 5000
+CAPTURE_WIDTH = 3000
+CAPTURE_HEIGHT = 3000
+DEVICE_SCALE_FACTOR = 3
 
 def render(html_path):
     chrome_options = Options()
@@ -24,22 +26,38 @@ def render(html_path):
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get(html_path)
-    driver.set_window_size(CAPTURE_WIDTH, CAPTURE_HEIGHT)
+    driver.execute_cdp_cmd("Emulation.setDeviceMetricsOverride", {
+        "mobile": False,
+        "width": CAPTURE_WIDTH,
+        "height": CAPTURE_HEIGHT,
+        "deviceScaleFactor": DEVICE_SCALE_FACTOR
+    })
 
-    element = WebDriverWait(driver, 20).until(
+    WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div.infographic-container"))
     )
 
-    bytes_ss = element.screenshot_as_png
-    infographic_img = Image.open(BytesIO(bytes_ss))
+    element = driver.find_element(By.CSS_SELECTOR, "div.infographic-container")
+    location = element.location_once_scrolled_into_view
+    size = element.size
+
+    screenshot_data = driver.get_screenshot_as_base64()
     driver.quit()
 
-    return infographic_img
+    full_img = Image.open(BytesIO(base64.b64decode(screenshot_data)))
+
+    left = int(location["x"] * DEVICE_SCALE_FACTOR)
+    top = int(location["y"] * DEVICE_SCALE_FACTOR)
+    right = int((location["x"] + size["width"]) * DEVICE_SCALE_FACTOR)
+    bottom = int((location["y"] + size["height"]) * DEVICE_SCALE_FACTOR)
+
+    cropped = full_img.crop((left, top, right, bottom))
+    return cropped
 
 # Testing-------------------------------------------------------
 if __name__ == "__main__":
     html_url = "sample_html.html"
-    html_path = "C:\\Users\\Liu Yijun\\Desktop\\Study\\FYP\\eCaption\\enh_news_info\\test\\" + html_url
+    html_path = "C:/Users/Liu Yijun/Desktop/Study/FYP/eCaption/enh_news_info/test/" + html_url
     
     infographic_img = render(html_path)
     infographic_img.save("enh_news_info/test/test_infographic.png")
